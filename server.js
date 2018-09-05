00d930e47bdb1b5bc229e407622eac01706a96f0
Guy <guy.mcauliffe@icloud.com>
2018-09-05 22:23:43 +0000

    Initial message coming from Server to Client

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js
 
 const express = require('express');
-const SocketServer = require('ws').Server;
+const WebSocket = require('ws');
+const WebSocketServer = WebSocket.Server;
+const uuid = require('uuid/v4');
 
 // Set the port to 3001
 const PORT = 3001;
@@ -13,7 +15,16 @@ const server = express()
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 // Create the WebSockets server
-const wss = new SocketServer({ server });
+const wss = new WebSocketServer({ server });
+
+// Broadcast helper, allows us to send a message to all connected clients.
+wss.broadcast = msg => {
+  wss.clients.forEach(client => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(msg);
+    }
+  });
+};
 
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
@@ -21,6 +32,36 @@ const wss = new SocketServer({ server });
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
+  // Initial message object to send to the connecting client.
+  const initialMessage = {
+    type: 'incomingNotification',
+    content: 'Welcome to Chatty!'
+  };
+
+  // Send the initial message to the client which just connected.
+  // This allows the react app to start with the right data.
+  ws.send(JSON.stringify(initialMessage));
+
+  // Message event handler, fires whenever a message from a client is received.
+  ws.on('message', message => {
+    console.log(`WS message ${message}`);
+    // Message payloads are all JSON, so parse into JS data structures.
+    const json = JSON.parse(message);
+    // Create a message to send back out
+    const outMsg = {
+      // Spread operator here copies all the key-value pairs from the json object into this object
+      ...json,
+      // Add an id
+      id: uuid(),
+      // Add a timestamp.
+      timestamp: new Date()
+    };
+    // Broadcast the change back to all clients.
+    wss.broadcast(JSON.stringify(outMsg));
+  });
+
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
 });
+
+console.log(`Socket Server is now listening at ws://localhost:${PORT}/`);
